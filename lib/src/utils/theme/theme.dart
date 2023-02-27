5dd836dc6fdeac347e87cc12b9e315ae95c68a0c
habertrogena <habertorogy12@gmail.com>
2023-02-27 14:12:00 +0300

    feat:added splash screen

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:loginui/src/utils/theme/widget_themes/text_theme.dart';
 
 class TAppTheme {
   TAppTheme._();
 
   static ThemeData lightTheme = ThemeData(
-      brightness: Brightness.light,
-      textTheme: TTextTheme.lightTextTheme,
-      elevatedButtonTheme:
-          ElevatedButtonThemeData(style: ElevatedButton.styleFrom()));
+    primarySwatch: Colors.green,
+    brightness: Brightness.light,
+    textTheme: TTextTheme.lightTextTheme,
+    appBarTheme: const AppBarTheme(),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(),
+    elevatedButtonTheme:
+        ElevatedButtonThemeData(style: ElevatedButton.styleFrom()),
+  );
 
-  static ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.green,
+    textTheme: TTextTheme.lightTextTheme,
+    appBarTheme: const AppBarTheme(),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(),
+    ),
+  );
 }
--- lib/src/utils/theme/widget_themes/text_theme.dart
+++ lib/src/utils/theme/widget_themes/text_theme.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TTextTheme {
-  static TextTheme lightTextTheme = TextTheme();
-  static TextTheme darkTextTheme = TextTheme();
+  static TextTheme lightTextTheme = TextTheme(
+    displayMedium: GoogleFonts.montserrat(color: Colors.black87),
+    titleSmall:
+        GoogleFonts.poppins(color: Colors.lightGreenAccent, fontSize: 24),
+  );
+
+  static TextTheme darkTextTheme = TextTheme(
+    displayMedium: GoogleFonts.montserrat(color: Colors.white70),
+    titleSmall: GoogleFonts.poppins(color: Colors.white60, fontSize: 24),
+  );
 }
