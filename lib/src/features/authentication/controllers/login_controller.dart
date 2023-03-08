cb6e5d4e6ca8fcb47c8fbc3c8607d10716f038b5
habertrogena <habertorogy12@gmail.com>
2023-03-08 15:53:14 +0300

    added validation

--- lib/src/features/authentication/controllers/login_controller.dart
+++ lib/src/features/authentication/controllers/login_controller.dart
@@ -1,8 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
+  static LoginController get instance => Get.find();
+
+  final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+  late TextEditingController emailController, passwordController;
+  var email = '';
+  var password = '';
   @override
   void onInit() {
     super.onInit();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+  }
+
+  @override
+  void onClose() {
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  //method to validate the email and password
+  String? validateEmail(String value) {
+    if (!GetUtils.isEmail(value)) {
+      return "provide a valid Email";
+    }
+    return null;
+  }
+
+  String? validatePassword(String value) {
+    if (value.length < 6) {
+      return "password must be of six characters";
+    }
+    return null;
+  }
+
+  void checkLogin() {
+    final isValid = loginFormKey.currentState!.validate();
+    if (!isValid) {
+      // return;
+      loginFormKey.currentState!.save();
+    }
+    // loginFormKey.currentState!.save();
   }
 }
--- lib/src/features/authentication/screens/login/login_form_widget.dart
+++ lib/src/features/authentication/screens/login/login_form_widget.dart
@@ -1,32 +1,38 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:loginui/src/features/authentication/controllers/login_controller.dart';
 import 'package:loginui/src/features/authentication/screens/forget_password/forget_password_options/forget_password_model_bottom_sheet.dart';
-import 'package:loginui/src/firebaseAuth/firebase_auth_service.dart';
 
 import '../../../../constants/sizes.dart';
 import '../../../../constants/text_string.dart';
 
 class LoginForm extends StatelessWidget {
-  LoginForm({
+  const LoginForm({
     super.key,
   });
 
-  final _formkey = GlobalKey<FormState>();
-  final FirebaseAuth_auth = FirebaseAuth.instance;
-
-  // text editing controllers
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+  // final FirebaseAuth_auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(LoginController());
+    // final _formKey = GlobalKey<FormState>();
     return Form(
+      key: controller.loginFormKey,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: tFormHeight - 10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextFormField(
+              controller: controller.emailController,
+              onSaved: (value) {
+                controller.email = value!;
+              },
+              validator: (value) {
+                return controller.validateEmail(value!);
+              },
               keyboardType: TextInputType.emailAddress,
               decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.person_outline_outlined),
@@ -40,6 +46,13 @@ class LoginForm extends StatelessWidget {
             ),
             TextFormField(
               keyboardType: TextInputType.visiblePassword,
+              controller: controller.passwordController,
+              onSaved: (value) {
+                controller.password = value!;
+              },
+              validator: (value) {
+                return controller.validatePassword(value!);
+              },
               obscureText: true,
               decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.fingerprint),
@@ -75,9 +88,17 @@ class LoginForm extends StatelessWidget {
               width: double.infinity,
               child: ElevatedButton(
                 onPressed: () async {
+                  // controller.checkLogin();
+
+                  if (controller.loginFormKey.currentState!.validate()) {
+                    //firebase logic
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processing Data')),
+                    );
+                  }
                   //test
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Processing data')));
+                  // ScaffoldMessenger.of(context).showSnackBar(
+                  //     const SnackBar(content: Text('Processing data')));
                 },
                 child: Text(
                   tLogin.toUpperCase(),
